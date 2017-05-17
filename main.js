cce8d72ec61f412bd89808bd54cc447cd0030101
Arnelle Balane <arnellebalane@gmail.com>
2017-05-17 17:59:37 +0800

    update media session metadata when playing new song

--- main.js
+++ main.js
@@ -11,23 +11,35 @@ source.connect(analyser);
 analyser.connect(audioCtx.destination);
 
 const playlist = [{
+    title: 'Tell Me If You Wanna Go Home',
+    url: 'audios/tell-me-if-you-wanna-go-home.mp3',
+    artist: 'Keira Knightley, Hailee Steinfeld',
+    album: 'Begin Again',
+    cover: 'images/cover.jpg'
+}, {
     title: 'A Step You Can\'t Take Back',
-    url: 'audios/a-step-you-cant-take-back.mp3'
+    url: 'audios/a-step-you-cant-take-back.mp3',
+    artist: 'Keira Knightley',
+    album: 'Begin Again',
+    cover: 'images/cover.jpg'
 }, {
     title: 'Coming Up Roses',
-    url: 'audios/coming-up-roses.mp3'
+    url: 'audios/coming-up-roses.mp3',
+    artist: 'Keira Knightley',
+    album: 'Begin Again',
+    cover: 'images/cover.jpg'
 }, {
     title: 'Lost Stars',
-    url: 'audios/lost-stars.mp3'
-}, {
-    title: 'Tell Me If You Wanna Go Home',
-    url: 'audios/tell-me-if-you-wanna-go-home.mp3'
+    url: 'audios/lost-stars.mp3',
+    artist: 'Adam Levine',
+    album: 'Begin Again',
+    cover: 'images/cover.jpg'
 }];
 
 let index = 0;
-play(index);
-
 audio.onended = next;
+
+document.onclick = () => play(0);
 
 function play(index) {
     if (index === undefined) {
@@ -36,11 +48,20 @@ function play(index) {
         stop();
         audio.src = playlist[index].url;
         audio.play();
+        updateMediaSession();
     }
 }
 
 function pause() {
     audio.pause();
+}
+
+function toggle() {
+    if (audio.paused) {
+        play();
+    } else {
+        pause();
+    }
 }
 
 function stop() {
@@ -56,4 +77,17 @@ function next() {
 function prev() {
     index = index ? index - 1 : playlist.length - 1;
     play(index);
+}
+
+function updateMediaSession() {
+    if ('mediaSession' in navigator) {
+        const metadata = Object.assign({}, playlist[index]);
+        metadata.artwork = [{
+            src: metadata.cover,
+            sizes: '128x128',
+            type: 'image/png'
+        }];
+        delete metadata.cover;
+        navigator.mediaSession.metadata = new MediaMetadata(metadata);
+    }
 }
